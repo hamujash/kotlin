52237929836c495a540c22444b6227d213c502bc
hamujash <y15111072@gmail.com>
2022-04-12 23:09:35 +0900

    キーボードを非表示

--- app/src/main/java/android/example/com/aboutme/MainActivity.kt
+++ app/src/main/java/android/example/com/aboutme/MainActivity.kt
@@ -1,11 +1,36 @@
 package android.example.com.aboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val doneButton = findViewById<Button>(R.id.done_button)
+        doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
